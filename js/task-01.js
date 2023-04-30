7d497427846f15c8f34dbbcff44e0e00a0d66679
Alona Adamska <alona.adamska@gmail.com>
2023-04-30 22:47:39 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,6 @@
-const categoriesList = document.querySelector('#categories');
-const categories = categoriesList.querySelectorAll('.item');
-
-console.log(`Number of categories: ${categories.length}`);
-
-categories.forEach(category => {
-  const categoryName = category.querySelector('h2').textContent;
-  const categoryItems = category.querySelectorAll('li').length;
-
-  console.log(`Category: ${categoryName} \nNumber of items: ${categoryItems}`);
-});
+const categoriesList = document.querySelectorAll('.item')
+console.log(`Number of categories:${categoriesList.length}`)
+categoriesList.forEach(e => {
+    console.log(`Category:${e.firstElementChild.textContent}`)
+    console.log(`Elements:${e.lastElementChild.querySelectorAll('li').length}`)
+})
